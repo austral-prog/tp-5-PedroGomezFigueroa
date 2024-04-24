5ef84efa58475e547fef0a581b3cf4ecee2d05ef
PedroGomezFigueroa <162721991+PedroGomezFigueroa@users.noreply.github.com>
2024-04-24 16:12:26 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,9 +1,13 @@
 def number_to_month(month):
+    
     mes = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
 
     if (month>=13) or (month==0):
+        
         return ("error")
+        
     else:
+        
         return mes[month-1]
        
 print(number_to_month(5))
