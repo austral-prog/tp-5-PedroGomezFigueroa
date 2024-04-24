47b0549aa260c8707d431fca9861e8923f0826f7
PedroGomezFigueroa <162721991+PedroGomezFigueroa@users.noreply.github.com>
2024-04-24 16:12:55 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,8 +1,11 @@
 def max_of_two(x, y):
     
     if (x>y):
+        
         return(x)
+        
     else:
+        
         return(y)
     
 print (max_of_two(1,2))
@@ -10,10 +13,15 @@ print (max_of_two(1,2))
 def max_of_three(x, y, z):
 
     if (x>y)and(x>z):
+        
         return(z)
+        
     elif(y>x)and(y>z):
+        
         return(y)
+        
     else:
+        
         return(z)
 
 print (max_of_three(5,6,7))
